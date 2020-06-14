43d7d7931ace0cb2f8fbe9d5755a6304d8f54921
Toru Shimogaisho <gaithimo@gmail.com>
2020-06-14 10:38:25 +0000

    Update pwa settings

--- next.config.js
+++ next.config.js
@@ -28,8 +28,33 @@ const withMDX = require("@next/mdx")({
     ],
   },
 })
-module.exports = withOffline(
-  withMDX({
+module.exports = withOffline({
+  ...withMDX({
     pageExtensions: ["tsx", "mdx"],
   }),
-)
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+})
