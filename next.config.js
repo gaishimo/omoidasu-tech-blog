f3235a6b96bb5b6f5fa0adfcf44d02b3fdd2d0ef
gaishimo <gaithimo@gmail.com>
2021-09-15 11:05:25 +0900

    Add prettier

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-const withPlugins = require('next-compose-plugins')
+const withPlugins = require("next-compose-plugins")
 const withMDX = require("@next/mdx")({
   extension: /\.mdx$/,
 })
 
 module.exports = withPlugins([withMDX], {
   pageExtensions: ["tsx", "mdx"],
-  webpack: (config) => {
+  webpack: config => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
-      'react-native$': 'react-native-web',
+      "react-native$": "react-native-web",
     }
     config.resolve.extensions = [
-      '.web.js',
-      '.web.ts',
-      '.web.tsx',
+      ".web.js",
+      ".web.ts",
+      ".web.tsx",
       ...config.resolve.extensions,
     ]
     return config
