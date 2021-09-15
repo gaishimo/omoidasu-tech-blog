ef458a94eb136f30a744a39bdc0d48a430447725
gaishimo <gaithimo@gmail.com>
2021-09-15 12:02:20 +0900

    Add rehype, remark plugins

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 const withPlugins = require("next-compose-plugins")
+// const withTM = require("next-transpile-modules")(["remark-slug"])
+const rehypePrism = require("@mapbox/rehype-prism")
+const remarkSlug = require("remark-slug")
+const remarkAutolinkHeadings = require("remark-autolink-headings")
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx$/,
+  options: {
+    rehypePlugins: [rehypePrism],
+    remarkPlugins: [
+      remarkSlug,
+      [
+        remarkAutolinkHeadings,
+        {
+          behavior: "prepend",
+          content: {
+            type: "element",
+            tagName: "img",
+            properties: {
+              alt: "header link",
+              src: "/linkGreyIcon.png",
+              className: ["header-link-icon"],
+            },
+          },
+        },
+      ],
+    ],
+  },
 })
 
 module.exports = withPlugins([withMDX], {
