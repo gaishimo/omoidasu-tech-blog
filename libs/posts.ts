e7b75e69134b087633560fec1c98aeeabe1d8e04
gaishimo <gaithimo@gmail.com>
2021-09-19 11:35:35 +0900

    .

--- libs/posts.ts
+++ libs/posts.ts
@@ -4,6 +4,7 @@ import * as path from "path"
 export async function getAllPosts(): Promise<Post[]> {
   try {
     const postsDir = path.resolve(process.cwd(), "pages", "posts")
+
     console.log("postsDir:", postsDir)
     const mdxFileNames = fs.readdirSync(postsDir)
     const posts = mdxFileNames
