986a929ce0af4bf1474731efd4df351314a01bdc
gaishimo <gaithimo@gmail.com>
2021-09-19 10:57:18 +0900

    Fix getAllPosts

--- libs/posts.ts
+++ libs/posts.ts
@@ -2,19 +2,26 @@ import * as fs from "fs"
 import * as path from "path"
 
 export async function getAllPosts(): Promise<Post[]> {
-  const mdxFileNames = fs.readdirSync(path.resolve(".", "pages", "posts"))
-  const posts = mdxFileNames
-    .map(fileName => {
-      const { meta } = require(`../pages/posts/${fileName}`)
-      return {
-        ...meta,
-        id: fileName.replace(/.mdx$/, ""),
-      }
-    })
-    .slice()
-    .sort(
-      (a: PostMeta, b: PostMeta) =>
-        b.createdAt.getTime() - a.createdAt.getTime(),
-    )
-  return posts
+  try {
+    const postsDir = path.resolve(process.cwd(), "pages", "posts")
+    console.log("postsDir:", postsDir)
+    const mdxFileNames = fs.readdirSync(postsDir)
+    const posts = mdxFileNames
+      .map(fileName => {
+        const { meta } = require(`../pages/posts/${fileName}`)
+        return {
+          ...meta,
+          id: fileName.replace(/.mdx$/, ""),
+        }
+      })
+      .slice()
+      .sort(
+        (a: PostMeta, b: PostMeta) =>
+          b.createdAt.getTime() - a.createdAt.getTime(),
+      )
+    return posts
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
 }
