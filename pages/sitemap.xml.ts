867cb5ac7e139ba81d3f2a6445c87075ee40e5fd
gaishimo <gaithimo@gmail.com>
2021-10-04 08:36:16 +0900

    Update sitemap

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -16,7 +16,13 @@ const BASE_URL = "https://blog.omoidasu.dev"
 
 export async function getServerSideProps({ req, res }: NextPageContext) {
   const posts = await getAllPosts()
-
+  const lastUpdated = posts
+    .map(post => post.lastUpdatedAt)
+    .reduce(
+      (a, b) => (a.getTime() <= b.getTime() ? b : a),
+      new Date(1900, 0, 1),
+    )
+  // const lastUpdated = new Date()
   const data = {
     declaration: {
       attributes: {
@@ -29,34 +35,62 @@ export async function getServerSideProps({ req, res }: NextPageContext) {
         type: "element",
         name: "urlset",
         attributes: { xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" },
-        elements: posts.map(post => ({
-          type: "element",
-          name: "url",
-          elements: [
-            {
-              type: "element",
-              name: "loc",
-              elements: [
-                { type: "text", text: `${BASE_URL}/posts/${post.id}` },
-              ],
-            },
-            {
-              type: "element",
-              name: "lastmod",
-              elements: [
-                {
-                  type: "text",
-                  text: format(post.lastUpdatedAt, "yyyy-MM-dd"),
-                },
-              ],
-            },
-            {
-              type: "element",
-              name: "priority",
-              elements: [{ type: "text", text: "0.5" }],
-            },
-          ],
-        })),
+        elements: [
+          {
+            type: "element",
+            name: "url",
+            elements: [
+              {
+                type: "element",
+                name: "loc",
+                elements: [{ type: "text", text: `${BASE_URL}` }],
+              },
+              {
+                type: "element",
+                name: "lastmod",
+                elements: [
+                  {
+                    type: "text",
+                    text: format(lastUpdated, "yyyy-MM-dd"),
+                  },
+                ],
+              },
+              {
+                type: "element",
+                name: "priority",
+                elements: [{ type: "text", text: "0.7" }],
+              },
+            ],
+          },
+          ...posts.map(post => ({
+            type: "element",
+            name: "url",
+            elements: [
+              {
+                type: "element",
+                name: "loc",
+                elements: [
+                  { type: "text", text: `${BASE_URL}/posts/${post.id}` },
+                ],
+              },
+              {
+                type: "element",
+                name: "lastmod",
+                elements: [
+                  {
+                    type: "text",
+                    text: format(post.lastUpdatedAt, "yyyy-MM-dd"),
+                  },
+                ],
+              },
+              {
+                type: "element",
+                name: "priority",
+                elements: [{ type: "text", text: "0.5" }],
+              },
+            ],
+          })),
+        ],
       },
     ],
     urlset: {
