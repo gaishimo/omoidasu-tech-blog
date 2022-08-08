78b70ca39eaca7354bae9a4757aff333e67b97ad
gaishimo <gaithimo@gmail.com>
2022-08-08 12:38:00 +0900

    Update post

--- components/pages/2022-08-08-reanimated-web-config/ReanimatedSample.tsx
+++ components/pages/2022-08-08-reanimated-web-config/ReanimatedSample.tsx
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
   box: {
     width: 100,
     height: 100,
-    borderRadius: 16,
+    borderRadius: 50,
     backgroundColor: "lightblue",
   },
   buttonWrapper: { marginTop: 24, width: 100 },
