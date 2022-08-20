89a3716cdeebf6b12f32633eed9d79dcf1bd3772
gaishimo <gaithimo@gmail.com>
2022-08-20 11:43:23 +0900

    Add post: 2022-08-20-rn-skia-shadow

---
+++ posts-meta/2022-08-20-rn-skia-shadow.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  title: "react-native-skiaでドロップシャドウをつける",
+  tagNames: ["react-native-skia", "シャドウ", "react-native"],
+  color1: "rgb(220, 220, 220)",
+  color2: "#5DAFEA",
+  createdAt: parse(
+    "2022-08-20 11:45:00 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description: "react-native-skiaでドロップシャドウをつける方法です。",
+  lastUpdatedAt: parse(
+    "2022-08-20 11:45:00 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
