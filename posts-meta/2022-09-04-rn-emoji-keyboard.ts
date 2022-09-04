27938c3ef843c40355101e15b2dffdd06190a342
gaishimo <gaithimo@gmail.com>
2022-09-04 16:05:25 +0900

    Add post: 2022-09-04-rn-emoji-keyboard

---
+++ posts-meta/2022-09-04-rn-emoji-keyboard.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "2022-09-04-rn-emoji-keyboard",
+  title: "react-nativeで絵文字を選択するためのライブラリrn-emoji-selector",
+  tagNames: ["react-native", "emoji", "ライブラリ"],
+  color1: "#A8DE52",
+  color2: "#ED6639",
+  createdAt: parse(
+    "2022-09-04 12:10:00 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description:
+    "先日のReact Native EU 2022で絵文字を選択するためのライブラリrn-emoji-keyboardが紹介されていました。",
+  imagePath: "/posts/2022-09-04-rn-emoji-keyboard/emoji-keyboard.png",
+  priority: 0.7,
+  lastUpdatedAt: parse(
+    "2022-09-04 12:10:00 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
