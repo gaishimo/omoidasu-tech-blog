34c86c762c33302937563dddd04794cb707278a4
gaishimo <gaithimo@gmail.com>
2022-09-12 08:38:57 +0900

    Update post: 2022-08-17-skia-arc

--- posts-meta/2022-08-17-skia-arc.ts
+++ posts-meta/2022-08-17-skia-arc.ts
@@ -12,9 +12,10 @@ exports.meta = {
     new Date(),
   ),
   author: "@gaishimo",
-  description: "react-native-skiaで円弧を描く方法についてです。",
+  description:
+    "react-native-skiaで円弧を描く方法についてです。path.addArcを使います。",
   lastUpdatedAt: parse(
-    "2022-08-17 09:15:00 +09:00",
+    "2022-09-12 08:40:00 +09:00",
     "yyyy-MM-dd HH:mm:ss XXX",
     new Date(),
   ),
