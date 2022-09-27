7a8eb1698c84053aaa642db003d22e8ffa1cc621
gaishimo <gaithimo@gmail.com>
2022-09-27 10:22:29 +0900

    Update post: 2022-08-08-reanimated-web-config

--- posts-meta/2022-08-08-reanimated-web-config.ts
+++ posts-meta/2022-08-08-reanimated-web-config.ts
@@ -2,6 +2,7 @@
 const { parse } = require("date-fns")
 
 exports.meta = {
+  id: "2022-08-08-reanimated-web-config",
   title: "Webでreact-native-reanimatedの設定を行う",
   tagNames: ["react-native-web", "next.js", "react-native-reanimated"],
   color1: "#233A9E",
@@ -13,9 +14,10 @@ exports.meta = {
   ),
   author: "@gaishimo",
   description:
-    "React Native Webでのreact-native-reanimatedの設定が意外と大変だったため、記録として残します。",
+    "React Native Web(Next.js)でreact-native-reanimatedの設定が意外と大変だったため、記録として残します。",
+  relatedPosts: ["2021-09-24-blog-rn-web", "2022-08-07-rn-skia-in-blog"],
   lastUpdatedAt: parse(
-    "2022-08-08 12:40:00 +09:00",
+    "2022-09-27 10:20:00 +09:00",
     "yyyy-MM-dd HH:mm:ss XXX",
     new Date(),
   ),
