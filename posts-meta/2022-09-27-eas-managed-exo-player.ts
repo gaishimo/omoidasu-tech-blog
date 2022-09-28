13ac18c658a01504f4ac39d6f5dc8214fde81cbb
gaishimo <gaithimo@gmail.com>
2022-09-28 09:40:44 +0900

    Add post: 2022-09-27-eas-managed-exo-player

---
+++ posts-meta/2022-09-27-eas-managed-exo-player.ts
@@ -0,0 +1,35 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "2022-09-27-eas-managed-exo-player",
+  title: "ExpoのManaged Workflowでreact-native-videoのExoPlayerを使う設定",
+  tagNames: [
+    "react-native",
+    "expo",
+    "EAS Build",
+    "Config Plugin",
+    "react-native-video",
+  ],
+  color1: "rgb(200, 200, 200)",
+  color2: "rgb(150, 150, 150)",
+  createdAt: parse(
+    "2022-09-28 09:45:35 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description: "react-native-videoのv5系でExoPlayerを使うための設定です。",
+  imagePath: null,
+  priority: 0.7,
+  relatedPosts: [
+    "2022-07-25-eas-widget",
+    "2021-08-29-eas-iap",
+    "2021-06-04-hello-eas-build-managed",
+  ],
+  lastUpdatedAt: parse(
+    "2022-09-28 09:45:35 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
