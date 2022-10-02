6eaa2f0edb516cc87d01581f28ceacfb5542fb17
gaishimo <gaithimo@gmail.com>
2022-10-02 11:07:29 +0900

    Add post: 2022-10-02-custom-haptic-ios

---
+++ posts-meta/2022-10-02-custom-haptic-ios.ts
@@ -0,0 +1,26 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "2022-10-02-custom-haptic-ios",
+  title: "React NativeでCustom Hapticsを使う (iOS)",
+  tagNames: ["react-native", "haptic", "custom haptic"],
+  color1: "#FF9956",
+  color2: "#CD2700",
+  createdAt: parse(
+    "2022-10-02 11:10:44 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description:
+    "React Nativeで独自のCustom Hapticsを使う方法について説明します(iOS)。",
+  imagePath: "/posts/2022-10-02-custom-haptic-ios/custom-haptics-ios.png",
+  priority: 0.7,
+  relatedPosts: [],
+  lastUpdatedAt: parse(
+    "2022-10-02 11:10:44 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
