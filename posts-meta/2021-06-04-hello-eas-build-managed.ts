181796bd5ad5acf539f4be7cc171137a85d1294a
gaishimo <gaithimo@gmail.com>
2022-10-07 10:45:04 +0900

    Update post: 2021-06-04-hello-eas-build-managed

--- posts-meta/2021-06-04-hello-eas-build-managed.ts
+++ posts-meta/2021-06-04-hello-eas-build-managed.ts
@@ -4,7 +4,7 @@ const { parse } = require("date-fns")
 exports.meta = {
   id: "2021-06-04-hello-eas-build-managed",
   title: "Hello EAS Build! (Managed Workflow)",
-  tagNames: ["expo", "react-native"],
+  tagNames: ["expo", "react-native", "EAS Build"],
   color1: "#6855F9",
   color2: "rgb(220, 220, 220)",
   createdAt: parse(
@@ -23,7 +23,7 @@ exports.meta = {
     "2022-09-27-eas-managed-exo-player",
   ],
   lastUpdatedAt: parse(
-    "2021-09-18 17:00:00 +09:00",
+    "2022-10-07 10:45:00 +09:00",
     "yyyy-MM-dd HH:mm:ss XXX",
     new Date(),
   ),
