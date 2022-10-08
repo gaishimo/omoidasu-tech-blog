6bf9fc8489b7f430bb991144dd9364b812895350
gaishimo <gaithimo@gmail.com>
2022-10-08 17:20:34 +0900

    Add post: 2022-10-08-config-plugin-dev-settings

---
+++ posts-meta/2022-10-08-config-plugin-dev-settings.ts
@@ -0,0 +1,32 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "2022-10-08-config-plugin-dev-settings",
+  title: "ExpoのConfig PluginをTypeScriptで開発する環境を整える",
+  tagNames: [
+    "react-native",
+    "expo",
+    "Config Plugin",
+    "EAS Build",
+    "typeScript",
+  ],
+  color1: "#D2CAFD",
+  color2: "#4630EB",
+  createdAt: parse(
+    "2022-10-08 17:20:20 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description:
+    "ExpoのConfig PluginをTypeScriptで開発するための環境設定について説明します。",
+  imagePath: null,
+  priority: 0.7,
+  relatedPosts: [],
+  lastUpdatedAt: parse(
+    "2022-10-08 17:20:20 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
