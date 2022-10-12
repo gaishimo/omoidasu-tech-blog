527f11194b8d3b7be8e0d2a3ce25b6187648fc20
gaishimo <gaithimo@gmail.com>
2022-10-12 12:08:26 +0900

    Add post: 2022-10-12-android-13-themed-icon

---
+++ posts-meta/2022-10-12-android-13-themed-icon.ts
@@ -0,0 +1,38 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "2022-10-12-android-13-themed-icon",
+  title: "ExpoのManaged WorkflowでAndroid 13のThemed Iconsを設定する",
+  tagNames: [
+    "react-native",
+    "expo",
+    "EAS Build",
+    "Config Plugin",
+    "Android13",
+    "Themed Icons",
+  ],
+  color1: "#D1D9B6",
+  color2: "#8BBE93",
+  createdAt: parse(
+    "2022-10-12 12:15:06 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description:
+    "ExpoのManaged WorkflowでAndroid 13のThemed Iconを設定する方法です。",
+  imagePath:
+    "/posts/2022-10-12-android-13-themed-icon/android-13-themed-icons.png",
+  priority: 0.7,
+  relatedPosts: [
+    "2022-10-08-config-plugin-dev-settings",
+    "2022-10-05-eas-managed-os-versions",
+    "2022-09-27-eas-managed-exo-player",
+  ],
+  lastUpdatedAt: parse(
+    "2022-10-12 12:15:06 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
