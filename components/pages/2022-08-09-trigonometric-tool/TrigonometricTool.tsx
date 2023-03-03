cca92fd45d9adbb3cf28e8d6cad61ab88ee0c5e2
gaishimo <gaithimo@gmail.com>
2023-03-04 08:52:10 +0900

    Fix skia error in 2022-08-09-trigonometric-tool

--- components/pages/2022-08-09-trigonometric-tool/TrigonometricTool.tsx
+++ components/pages/2022-08-09-trigonometric-tool/TrigonometricTool.tsx
@@ -2,9 +2,7 @@ import {
   Canvas,
   Circle,
   DashPathEffect,
-  Paint,
   Path,
-  usePaintRef,
 } from "@shopify/react-native-skia"
 import { useCallback, useMemo, useState } from "react"
 import { StyleSheet, Text, TextInput, View } from "react-native"
@@ -15,8 +13,6 @@ export default function TrigonometricTool() {
   const [toolWidth, setToolWidth] = useState(340)
   const center = { x: toolWidth / 2, y: 200 }
 
-  const xyLineRef = usePaintRef()
-  const supportLineRef = usePaintRef()
   const [radius, setRadius] = useState<number | null>(100)
   const [degree, setDegree] = useState<number | null>(60)
 
@@ -34,26 +30,41 @@ export default function TrigonometricTool() {
     return { sin: sin.toFixed(3), cos: cos.toFixed(3) }
   }, [radius, degree])
 
+  const xyLineProps = {
+    color: "rgb(150, 150, 150)",
+    style: "stroke" as const,
+    strokeWidth: 0.5,
+  }
+  const supportLineProps = {
+    color: "lightblue",
+    style: "stroke" as const,
+    strokeWidth: 1.5,
+  }
+
   return (
     <View style={{ width: toolWidth }}>
       <Canvas style={[styles.canvas]}>
         {/* X, Y軸 */}
-        <Paint
+        {/* <Paint
           ref={xyLineRef}
           color="rgb(150, 150, 150)"
           style="stroke"
           strokeWidth={0.5}
         >
           <DashPathEffect intervals={[3, 3]} />
-        </Paint>
+        </Paint> */}
         <Path
-          paint={xyLineRef}
+          {...xyLineProps}
           path={`M 0 ${center.y} L ${toolWidth} ${center.y}`}
-        />
+        >
+          <DashPathEffect intervals={[3, 3]} />
+        </Path>
         <Path
-          paint={xyLineRef}
+          {...xyLineProps}
           path={`M ${toolWidth / 2} 0 L ${toolWidth / 2} ${canvasHeight}`}
-        />
+        >
+          <DashPathEffect intervals={[3, 3]} />
+        </Path>
 
         {/* 中心から伸びる線 (角度により変わる) */}
         <Path
@@ -64,20 +75,20 @@ export default function TrigonometricTool() {
         />
 
         {/* sinとcosの補助線 */}
-        <Paint
+        {/* <Paint
           ref={supportLineRef}
           color="lightblue"
           style="stroke"
           strokeWidth={1.5}
         >
           <DashPathEffect intervals={[3, 3]} />
-        </Paint>
+        </Paint> */}
         <Path
-          paint={supportLineRef}
+          {...supportLineProps}
           path={`M ${lineEndPos.x} ${lineEndPos.y} ${lineEndPos.x} ${center.y}`}
         />
         <Path
-          paint={supportLineRef}
+          {...supportLineProps}
           path={`M ${lineEndPos.x} ${lineEndPos.y} ${center.x} ${lineEndPos.y}`}
         />
 
