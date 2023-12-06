ccb86fc6e663315909f6a1e7883d1e09b384975c
gaishimo <gaithimo@gmail.com>
2023-12-06 15:53:16 +0900

    Add article 2023-12-06-expo-modules

---
+++ posts-meta/2023-12-06-expo-modules.ts
@@ -0,0 +1,32 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "expo-modules",
+  title: "Expo Modulesとは?",
+  tagNames: [
+    "react-native",
+    "expo",
+    "expo-modules",
+    "react",
+  ],
+  color1: "#80CBC4",
+  color2: "rgb(220, 220, 220)",
+  createdAt: parse(
+    "2023-12-06 15:50:00 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description:
+    "React Nativeのネイティブモジュールを作成する仕組みであるExpo Modulesの概要について説明します。",
+  imagePath: null,
+  priority: 0.7,
+  relatedPosts: [],
+  lastUpdatedAt: parse(
+    "2023-12-06 15:50:00 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
+
