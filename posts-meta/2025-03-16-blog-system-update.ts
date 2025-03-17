bf94019ff54fda75499df9e5899e0f28fd050cdf
gaishimo <gaithimo@gmail.com>
2025-03-17 09:27:52 +0900

    Add post: 2025-03-16-blog-system-update

---
+++ posts-meta/2025-03-16-blog-system-update.ts
@@ -0,0 +1,36 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "2025-03-16-blog-system-update",
+  title: "当ブログのシステムを更新しました",
+  tagNames: [
+    "react-native",
+    "skia",
+    "react-native-skia",
+    "react-native-reanimated",
+    "react",
+    "アニメーション",
+    "blog",
+    "Cursor",
+    "Next.js",
+    "react-native-web",
+  ],
+  color1: "rgb(67, 134, 255)",
+  color2: "rgb(255, 89, 123)",
+  createdAt: parse(
+    "2025-03-16 20:22:59 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description: "記事に関する概要を記載",
+  imagePath: null,
+  priority: 0.7,
+  relatedPosts: [],
+  lastUpdatedAt: parse(
+    "2025-03-16 20:22:59 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
