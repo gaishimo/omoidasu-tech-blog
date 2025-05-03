1178b3067aeae58c648bba78ba54e23062da28a5
gaishimo <gaithimo@gmail.com>
2025-05-03 13:48:30 +0900

    Add post: 2025-05-01-expo-apple-targets

---
+++ posts-meta/2025-05-01-expo-apple-targets.ts
@@ -0,0 +1,38 @@
+// @ts-ignore
+const { parse } = require("date-fns")
+
+exports.meta = {
+  id: "2025-05-01-expo-apple-targets",
+  title: "expo-apple-targetsの凄さ",
+  tagNames: [
+    "react-native",
+    "expo",
+    "config-plugin",
+    "ios",
+    "xcode",
+    "pbxproj",
+  ],
+  color1: "#2196F3",
+  color2: "#FF9800",
+  createdAt: parse(
+    "2025-05-01 14:26:37 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+  author: "@gaishimo",
+  description:
+    "iOSのWidgetやApp Clip等のターゲットをManaged Workflowで利用するためのConfig Pluginであるexpo-apple-targetsが凄かったため、それについて書きます。",
+  imagePath: null,
+  priority: 0.7,
+  relatedPosts: [
+    "2022-07-25-eas-widget",
+    "2023-12-06-expo-modules",
+    "2022-10-08-config-plugin-dev-settings",
+    "2022-10-16-eas-config-plugin-podfile",
+  ],
+  lastUpdatedAt: parse(
+    "2025-05-01 14:26:37 +09:00",
+    "yyyy-MM-dd HH:mm:ss XXX",
+    new Date(),
+  ),
+}
